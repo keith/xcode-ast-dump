f7e6a51f82f4b38acc50f9ebd4eae3dfaa5f0264
Keith Smiley <keithbsmiley@gmail.com>
2017-02-25 18:09:01 -0800

    Add smarter swiftc executable picking

--- ast.py
+++ ast.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import subprocess
+import sys
 
 
-def ast_command(arguments):
-    return ["swiftc", "-dump-ast"] + arguments
+def swiftc_executable(environment=None):
+    environment = environment or os.environ
+
+    try:
+        return environment["AST_SWIFTC"]
+    except KeyError:
+        pass
+
+    try:
+        developer_dir = environment["DEVELOPER_DIR"]
+        toolchain = environment["TOOLCHAINS"].split(".")[-1] + ".xctoolchain"
+        return os.path.join(developer_dir, "Toolchains", toolchain,
+                            "usr/bin/swiftc")
+    except KeyError:
+        return "swiftc"
+
+
+def is_in_xcode(environment=None):
+    environment = environment or os.environ
+    return environment.get("TOOLCHAINS") is not None
+
+
+def ast_command(swiftc, arguments):
+    return [swiftc, "-dump-ast"] + arguments
 
 
 def build_parser():
@@ -19,8 +43,18 @@ def build_parser():
     parser.add_argument("-parseable-output", action="store_true")
     return parser
 
-if __name__ == "__main__":
+
+def main():
     _, other_arguments = build_parser().parse_known_args()
-    command = ast_command(other_arguments)
-    print(" ".join(command))
+    command = ast_command(swiftc_executable(), other_arguments)
+    if is_in_xcode():
+        print(" ".join(command))
+        print("\nAST:\n")
+        sys.stdout.flush()
     print(subprocess.check_output(command))
+    if is_in_xcode():
+        print("Exiting with 1 to stop the build")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
--- tests/test_ast.py
+++ tests/test_ast.py
@@ -11,13 +11,39 @@ class TestAST(unittest.TestCase):
             self.assertNotIn("emit", argument)
 
     def test_adds_dump(self):
-        command = ast.ast_command([])
+        command = ast.ast_command("", [])
         self.assertIn("-dump-ast", command)
 
-    def test_starts_with_swiftc(self):
-        command = ast.ast_command([])
-        self.assertEqual(command[0], "swiftc")
+    def test_starts_with_swiftc_argument(self):
+        command = ast.ast_command("swiftc_arg", [])
+        self.assertEqual(command[0], "swiftc_arg")
 
     def test_ends_with_other_arguments(self):
-        command = ast.ast_command(["foobar"])
+        command = ast.ast_command("", ["foobar"])
         self.assertEqual(command[2], "foobar")
+
+    def test_swiftc_executable_path(self):
+        environment = {"DEVELOPER_DIR": "/foo", "TOOLCHAINS": "com.bar"}
+        swiftc = ast.swiftc_executable(environment)
+        self.assertEqual(swiftc,
+                         "/foo/Toolchains/bar.xctoolchain/usr/bin/swiftc")
+
+    def test_swiftc_executable_path_override(self):
+        environment = {"DEVELOPER_DIR": "/foo", "TOOLCHAINS": "com.bar",
+                       "AST_SWIFTC": "bar"}
+        swiftc = ast.swiftc_executable(environment)
+        self.assertEqual(swiftc, "bar")
+
+    def test_swift_executable_without_xcode(self):
+        swiftc = ast.swiftc_executable({"AST_SWIFTC": "baz"})
+        self.assertEqual(swiftc, "baz")
+
+    def test_swift_executable_no_environ(self):
+        swiftc = ast.swiftc_executable({})
+        self.assertEqual(swiftc, "swiftc")
+
+    def test_is_in_xcode(self):
+        self.assertTrue(ast.is_in_xcode({"TOOLCHAINS": "is_set"}))
+
+    def test_is_not_in_xcode(self):
+        self.assertFalse(ast.is_in_xcode({}))
